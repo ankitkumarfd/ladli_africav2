bbdf04a4c834718bdc92820d2e91986779aec9b4
Raj <rajladli520@gmail.com>
2025-09-02 17:42:42 +0530

    Implemented favicon and resolve the issue of preview while sharing the website link

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 docker command
 
-docker build -t webladli/ladliafrica:0.0.4 .
+docker build -t webladli/ladliafrica:0.0.7 .
 
-docker container run -d -p 3000:3000 webladli/ladliafrica:0.0.4
+docker container run -d -p 3000:3000 webladli/ladliafrica:0.0.7
 
 docker container stop
 
-docker push webladli/ladliafrica:0.0.4
+docker push webladli/ladliafrica:0.0.7
--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,6 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from "../../components/Header";
 import Footer from "../../components/Footer";
-import ladliAfricaLogo from "@/public/ladliAfricaLogo.png";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +24,16 @@ export const metadata = {
     title: "Ladli Foundation Africa | Advancing Health & Education",
     description:
       "Explore Ladli Foundation Africa delivering health, education, WASH, and protection programs empowering women and youth through partnerships with dignity",
-    images: {ladliAfricaLogo},
+    images: [
+      {
+        // url: "https://africa.ladlifoundation.org/ladliAfricaLogo.png",
+        // url: "https://africa.ladlifoundation.org/africaLogo.png",
+        url: "https://africa.ladlifoundation.org/africaLogoPreview.png",
+        width: 100,
+        height: 100,
+        alt: "Ladli Foundation Africa Logo",
+      },
+    ],
     url: "https://africa.ladlifoundation.org/",
     type: "website",
   },
